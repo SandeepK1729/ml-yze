219f135b9dd71ee8e574d7a30657e654f2742875
Konda Sandeep <82932586+SandeepK1729@users.noreply.github.com>
2022-07-09 21:54:39 +0530

    update app.py

--- app.py
+++ app.py
@@ -23,11 +23,13 @@ from sklearn.metrics import accuracy_score, precision_score,confusion_matrix
 app=Flask(__name__)
 
 #model initialization
-gnb = GaussianNB()
-svm = SVC()
-dt = tree.DecisionTreeClassifier()
-rf = RandomForestClassifier(n_estimators = 25)
-ada = AdaBoostClassifier()
+clf = {
+    "naivebayes" : GaussianNB(), 
+    "svm" : SVC(),
+    "decisiontree" : tree.DecisionTreeClassifier(),
+    "randomforest" : RandomForestClassifier(n_estimators = 25), 
+    "adaboost" : AdaBoostClassifier(), 
+} 
 
 @app.route('/')
 def hello_world():
@@ -56,82 +58,22 @@ def entry_point():
         elif dataset=="strokes":
             df = pd.read_csv("csvfiles/strokes.csv")
             y = df['stroke']
-            x = df.drop("stroke",axis = 1)
-            X_train, X_test,y_train,y_test = train_test_split(x,y,test_size = 0.25)
+            X = df.drop("stroke",axis = 1)
+            
         elif dataset=="wine":
             df = datasets.load_wine()
             y = df.target
-            x = df.data
-            X_train, X_test,y_train,y_test = train_test_split(x,y,test_size = 0.25)
-        if classifier=="naivebayes":
-            gnb.fit(X_train,y_train)
-            y_pred_nb = gnb.predict(X_test)
-            accuracy = accuracy_score(y_test,y_pred_nb)
-            precisons=precision_score(y_test,y_pred_nb,average='weighted')
-            cm=confusion_matrix(y_test,y_pred_nb)
-            heatmap = sns.heatmap(cm, annot=True, cmap="Blues")
-            plt.ylabel('True label')
-            plt.xlabel('Predicted label')
-            plt.title('Heapmap for confusion matrix')
-            img = io.BytesIO()
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-            #print(accuracy)
-            #print(precisons)
-            #print(cm)
-        elif classifier=="svm":
-            svm.fit(X_train,y_train)
-            y_pred_svm=svm.predict(X_test)
-            accuracy=accuracy_score(y_test,y_pred_svm)
-            precisons=precision_score(y_test,y_pred_svm,average='weighted')
-            cm=confusion_matrix(y_test,y_pred_svm)
-            heatmap = sns.heatmap(cm, annot=True, cmap="Blues")
-            plt.ylabel('True label')
-            plt.xlabel('Predicted label')
-            plt.title('Heapmap for confusion matrix')
-            img = io.BytesIO()
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-        elif classifier=="decisiontree":
-            dt.fit(X_train,y_train)
-            y_pred_dt=dt.predict(X_test)
-            accuracy=accuracy_score(y_test,y_pred_dt)
-            precisons=precision_score(y_test,y_pred_dt,average='weighted')
-            cm=confusion_matrix(y_test,y_pred_dt)
-            heatmap = sns.heatmap(cm, annot=True, cmap="Blues")
-            plt.ylabel('True label')
-            plt.xlabel('Predicted label')
-            plt.title('Heapmap for confusion matrix')
-            img = io.BytesIO()
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-        elif classifier=="randomforest":
-            rf.fit(X_train,y_train)
-            y_pred_rf = rf.predict(X_test)
-            accuracy = accuracy_score(y_test,y_pred_rf)
-            precisons=precision_score(y_test,y_pred_rf,average='weighted')
-            cm=confusion_matrix(y_test,y_pred_rf)
-            heatmap = sns.heatmap(cm, annot=True, cmap="Blues")
-            plt.ylabel('True label')
-            plt.xlabel('Predicted label')
-            plt.title('Heapmap for confusion matrix')
-            img = io.BytesIO()
-            plt.savefig(img, format='png')
-            plt.close()
-            img.seek(0)
-            plot_url = base64.b64encode(img.getvalue()).decode('utf8')
-        elif classifier=="adaboost":
-            ada.fit(X_train,y_train)
-            y_pred_ada=ada.predict(X_test)
-            accuracy=accuracy_score(y_test,y_pred_ada)
-            precisons=precision_score(y_test,y_pred_ada,average='weighted')
-            cm=confusion_matrix(y_test,y_pred_ada)
+            X = df.data
+        X_train, X_test,y_train,y_test = train_test_split(x,y,test_size = 0.25)
+        
+        if classifier in clf: 
+            model = clf[classifier] 
+            
+            model.fit(X_train,y_train)
+            y_pred = model.predict(X_test)
+            accuracy = accuracy_score(y_test,y_pred )
+            precisons=precision_score(y_test,y_pred,average='weighted')
+            cm=confusion_matrix(y_test,y_pred)
             heatmap = sns.heatmap(cm, annot=True, cmap="Blues")
             plt.ylabel('True label')
             plt.xlabel('Predicted label')
